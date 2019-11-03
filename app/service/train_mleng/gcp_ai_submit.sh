b4ac2d2592f28dfbb51efc513b91ea000fa3393e
Dmitry Kisler <admin@dkisler.de>
2019-11-04 00:45:10 +0100

    add scheduler

--- app/service/train_mleng/gcp_ai_submit.sh
+++ app/service/train_mleng/gcp_ai_submit.sh
@@ -4,13 +4,17 @@ VERSION=$1
 PROJECT_ID=sellics
 REGION=us-central1
 
-JOB_NAME=${PROJECT_ID}_$(TZ=":UTC" date +%Y%m%dT%H%M%SZ)
 
-gcloud ai-platform jobs submit training ${JOB_NAME} \
-  --master-image-uri gcr.io/${PROJECT_ID}/service/trainer:${VERSION} \
-  --region ${REGION} \
-  --config config.yaml \
-  -- \
-  --data-path=train/${VERSION}/data_computer.pkl \
-  --config-path=${VERSION}/params_test.yaml \
-  --model-dir=${VERSION}/$(TZ=":UTC" date +%Y/%m/%d/%H)
+platforms=(computer smartphone tablet)
+for platform in ${platforms[@]}; do
+  JOB_NAME=${PROJECT_ID}_${platform}_$(TZ=":UTC" date +%Y%m%dT%H%M%SZ)
+
+  gcloud ai-platform jobs submit training ${JOB_NAME} \
+    --master-image-uri gcr.io/${PROJECT_ID}/service/trainer:${VERSION} \
+    --region ${REGION} \
+    --config config.yaml \
+    -- \
+    --data-path=train/${VERSION}/data_${platform}.pkl \
+    --config-path=${VERSION}/params.yaml \
+    --model-dir=${VERSION}/${platform}/$(TZ=":UTC" date +%Y/%m/%d/%H)
+done
