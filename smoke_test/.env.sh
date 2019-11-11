bc4246f491effdb563669b0aee6afdf3369a72c6
Dmitry Kisler <admin@dkisler.de>
2019-11-11 02:01:31 +0100

    aligned smoke test settings with runner

--- smoke_test/.env.sh
+++ smoke_test/.env.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+DATA_VER=features_v1
 PREFIX=$(date +'%Y/%m/%d')/e2e_test
 
 export BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null && pwd )"
@@ -10,12 +11,12 @@ EXPECTED_OUTPUT="6,Computer,0"
 export MODEL_VER="v1"
 export BUCKET_MODEL=${BASE_DIR}/bucket/model
 # train service
-export MODEL_PREFIX=${PREFIX}/${MODEL_VER} 
-export BUCKET_DATA_TRAIN=${BASE_DIR}/bucket/data/train
+export MODEL_PREFIX=${MODEL_VER}/${PREFIX}
+export BUCKET_DATA_TRAIN=${BASE_DIR}/bucket/data/train/${DATA_VER}
 export PATH_TRAIN=${PREFIX}/train.csv.gz
 export PATH_EVAL=${PREFIX}/eval.csv.gz
 # serve service
-export PATH_MODEL=${PREFIX}/${MODEL_VER}/model.pkl
+export PATH_MODEL=${MODEL_VER}/${PREFIX}/model.pkl
 export BUCKET_DATA_PREDICT=${BASE_DIR}/bucket/data/predict
-export PATH_DATA_IN=input/${PREFIX}/prediction_input.csv.gz
-export PATH_DATA_OUT=output/${PREFIX}/prediction_output.csv.gz
+export PATH_DATA_IN=input/${DATA_VER}/${PREFIX}/prediction_input.csv.gz
+export PATH_DATA_OUT=output/${DATA_VER}/${PREFIX}/prediction_output.csv.gz
